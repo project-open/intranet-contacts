46322dbdbaa61e39009cc77b8214ce3afc87e1f9
Project Open <info@project-open.com>
2021-01-07 22:48:24 +0100

    - Fixed	acs_rel_type__create_type

--- sql/postgresql/contacts-list-create.sql
+++ sql/postgresql/contacts-list-create.sql
@@ -126,7 +126,8 @@ select acs_rel_type__create_type (
         'acs_object', 
         null,
         0, 
-        null
+        null,
+	true
     );
 
 select define_function_args('contact_owner__new','rel_id,rel_type;contact_owner,object_id_one,object_id_two,creation_user,creation_ip');
